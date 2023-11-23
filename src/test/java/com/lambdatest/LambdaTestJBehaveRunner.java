6c5b53d408651dec19a9d98175c296209a52e0d6
PARAG JAIN <87814011+paragjain15@users.noreply.github.com>
2023-11-23 22:33:48 +0530

    Added support for calling Dynamic App_ID

--- src/test/java/com/lambdatest/LambdaTestJBehaveRunner.java
+++ src/test/java/com/lambdatest/LambdaTestJBehaveRunner.java
@@ -27,84 +27,80 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-@RunWith(Parameterized.class) public class LambdaTestJBehaveRunner {
+@RunWith(Parameterized.class)
+public class LambdaTestJBehaveRunner {
 
-  public WebDriver driver;
-  // private Local l;
+    public WebDriver driver;
+    // private Local l;
 
-  private static JSONObject config;
+    private static JSONObject config;
 
-  @Parameter(value = 0) public int taskID;
+    @Parameter(value = 0)
+    public int taskID;
 
-  @Parameters
-  public static Collection<Object[]> data() throws Exception {
-    List<Object[]> taskIDs = new ArrayList<Object[]>();
-    if (System.getProperty("config") != null) {
-      JSONParser parser = new JSONParser();
-      config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + System.getProperty("config")));
-      int envs = ((JSONArray) config.get("environments")).size();
+    @Parameters
+    public static Collection<Object[]> data() throws Exception {
+        List<Object[]> taskIDs = new ArrayList<Object[]>();
+        if (System.getProperty("config") != null) {
+            JSONParser parser = new JSONParser();
+            config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + System.getProperty("config")));
+            int envs = ((JSONArray) config.get("environments")).size();
 
-      for (int i = 0; i < envs; i++) {
-        taskIDs.add(new Object[] { i });
-      }
+            for (int i = 0; i < envs; i++) {
+                taskIDs.add(new Object[]{i});
+            }
+        }
+
+        return taskIDs;
     }
 
-    return taskIDs;
-  }
+    @Before
+    public void setUp() throws Exception {
+        JSONArray envs = (JSONArray) config.get("environments");
+        String username = System.getenv("LT_USERNAME") == null ? "YOUR_LT_USERNAME" : System.getenv("LT_USERNAME");  //Replace YOUR_LT_USERNAME with your LambdaTest username
 
-  @Before
-  public void setUp() throws Exception {
-    JSONArray envs = (JSONArray) config.get("environments");
-    String username = System.getenv("LT_USERNAME") == null ?
-      "YOUR_LT_USERNAME" :
-      System.getenv("LT_USERNAME");  //Replace YOUR_LT_USERNAME with your LambdaTest username
+        String accessKey = System.getenv("LT_ACCESS_KEY") == null ? "YOUR_LT_ACCESS_KEY" : System.getenv("LT_ACCESS_KEY"); //Replace YOUR_LT_ACCESS_KEY with your LambdaTest accessKey
 
-    String accessKey = System.getenv("LT_ACCESS_KEY") == null ?
-      "YOUR_LT_ACCESS_KEY" :
-      System.getenv("LT_ACCESS_KEY"); //Replace YOUR_LT_ACCESS_KEY with your LambdaTest accessKey
+        String app_id = System.getenv("LT_APP_ID") == null ? "lt://proverbial-android" : System.getenv("LT_APP_ID");      //Enter your LambdaTest App ID at the place of lt://proverbial-android
 
-    String app_id = System.getenv("LT_APP_ID") == null ?
-      "lt://proverbial-android" :
-      System.getenv("LT_APP_ID");      //Enter your LambdaTest App ID at the place of lt://proverbial-android
+        String grid_url = System.getenv("LT_GRID_URL") == null ? "mobile-hub.lambdatest.com" : System.getenv("LT_GRID_URL");
 
-    String grid_url = System.getenv("LT_GRID_URL") == null ? "mobile-hub.lambdatest.com" : System.getenv("LT_GRID_URL");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("isRealMobile", true);
+        capabilities.setCapability("app", app_id);    //Enter app_url here
 
-    DesiredCapabilities capabilities = new DesiredCapabilities();
-    capabilities.setCapability("isRealMobile", true);
-    capabilities.setCapability("app", app_id);    //Enter app_url here
+        Map<String, String> envCapabilities = (Map<String, String>) envs.get(taskID);
+        Iterator it = envCapabilities.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
+        }
 
-    Map<String, String> envCapabilities = (Map<String, String>) envs.get(taskID);
-    Iterator it = envCapabilities.entrySet().iterator();
-    while (it.hasNext()) {
-      Map.Entry pair = (Map.Entry) it.next();
-      capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
+        Map<String, String> commonCapabilities = (Map<String, String>) config.get("capabilities");
+        it = commonCapabilities.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            if (capabilities.getCapability(pair.getKey().toString()) == null) {
+                capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
+            }
+        }
+
+        driver = new RemoteWebDriver(new URL("http://" + username + ":" + accessKey + "@" + grid_url + "/wd/hub"),
+                capabilities);
     }
 
-    Map<String, String> commonCapabilities = (Map<String, String>) config.get("capabilities");
-    it = commonCapabilities.entrySet().iterator();
-    while (it.hasNext()) {
-      Map.Entry pair = (Map.Entry) it.next();
-      if (capabilities.getCapability(pair.getKey().toString()) == null) {
-        capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
-      }
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
     }
 
-    driver = new RemoteWebDriver(new URL("http://" + username + ":" + accessKey + "@" + grid_url + "/wd/hub"),
-      capabilities);
-  }
+    @Test
+    public void runStories() throws Exception {
+        Class<?> c = Class.forName(System.getProperty("embedder"));
+        Constructor<?> cons = c.getConstructor(WebDriver.class);
+        Embedder storyEmbedder = (Embedder) cons.newInstance(driver);
 
-  @After
-  public void tearDown() throws Exception {
-    driver.quit();
-  }
-
-  @Test
-  public void runStories() throws Exception {
-    Class<?> c = Class.forName(System.getProperty("embedder"));
-    Constructor<?> cons = c.getConstructor(WebDriver.class);
-    Embedder storyEmbedder = (Embedder) cons.newInstance(driver);
-
-    List<String> storyPaths = Arrays.asList(System.getProperty("stories"));
-    storyEmbedder.runStoriesAsPaths(storyPaths);
-  }
+        List<String> storyPaths = Arrays.asList(System.getProperty("stories"));
+        storyEmbedder.runStoriesAsPaths(storyPaths);
+    }
 }
